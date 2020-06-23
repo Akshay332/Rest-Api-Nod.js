be1eea31f617b21dd13ebd0e281fc929a1049ba7
Akshay332 <munnapathania475@gmail.com>
2020-06-23 16:44:09 +0530

    modified app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use('/api/admin', admin)
 
 //Bring in the hotels routes 
 const product = require('./routes/product')
-app.use('/api/hotels', product)
+app.use('/api/products', product)
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
