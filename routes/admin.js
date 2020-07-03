4e9fb0dc06ed5c68da0f6c7399a59686147ad00a
Akshay332 <munnapathania475@gmail.com>
2020-07-03 13:31:39 +0530

    admin routes modified

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 const Admin = require('../model/admin')
 const auth = require('../middleware/adminAuth')
 
