afa3b07786cfac54041bb5473dae7b246ae6b5ee
Akshay332 <munnapathania475@gmail.com>
2020-07-03 13:29:22 +0530

    contact us model created

---
+++ model/contactus.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const contactusSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid!')
+            }
+        }
+    },
+    phoneNumber: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    enquiry: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('hotel', Hotel);
